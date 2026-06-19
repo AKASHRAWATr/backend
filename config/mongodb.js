7f6b751c39585e846aedfc16876fef7737dc9ff2
AKASHRAWATr <akashrawatr697@gmail.com>
2026-06-19 11:21:21 +0530

    changes in .env and configure

--- config/mongodb.js
+++ config/mongodb.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 async function connectDB() {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/firstTestingDB");
+    // await mongoose.connect("mongodb://127.0.0.1:27017/firstTestingDB");
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("Connected to DB");
   } catch (error) {
     console.log("Error in Connecting to DB", error);
